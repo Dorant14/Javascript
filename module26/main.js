c2487327335d6967ad170b0b9d8f095d9c2117ac
Dorant14 <dorant.hajdari12@gmail.com>
2024-10-03 16:17:23 +0200

    hhhhhhhhhhhhh

---
+++ module26/main.js
@@ -0,0 +1,32 @@
+var name="pppppppppp"
+var age="13.9999999999999999999999999"
+var truth= false
+
+var x= 10 + 30
+var y= 15 - 5
+var z= 15/17
+var a= 120 * 7
+var a= 120 ** 7
+
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(f)
+console.log(e)
+
+a+=15
+console.log(a)
+
+a++;
+
+console.log(a)
+
+var numri1 = "20"
+var numri2 = 20
+
+console.log(numri1 == numri2)
+console.log(numri1 === numri2)
+
+console.log(!(numri1 === numri2))
