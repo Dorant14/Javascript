018e64653a38ed8fa71d5d208ea890976fc0b591
Dorant14 <dorant.hajdari12@gmail.com>
2024-10-08 16:16:04 +0200

    aaaaaaaaa

---
+++ module27/main.js
@@ -0,0 +1,17 @@
+console.log('digital school')
+
+var a = 2
+var r = 9
+var d = 4
+var e = 3
+
+console.log( r < a && r > t )
+console.log( r <= a && r > t )
+console.log( r < a && r > t )
+console.log( r < a && r > t )
+
+var school = "Digital School"
+
+console.log('My schools nname is ${school}')
+
+console.log("My schools nname is" + school)
