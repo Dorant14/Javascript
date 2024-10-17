10668ff4e113d2e19e65595ebd039c21e6b3d182
Dorant14 <dorant.hajdari12@gmail.com>
2024-10-17 16:17:49 +0200

    bombastic

---
+++ module29/main.js
@@ -0,0 +1,16 @@
+var programming_lang =["RDR","RDR2","GtaV","Gat","Rust","Fortnight","Fall guys","Minecraft"];
+
+console.log(programilng_lang[1]);
+
+programming_lang.push = ["Red Dead Redemption 2"];
+
+console.log(programming_lang);
+
+programming_lang.pop();
+
+console.log(programming_lang);
+
+programming_lang.shift["Skibidi toilet tower defence"];
+
+console.log(programming_lang);
+
