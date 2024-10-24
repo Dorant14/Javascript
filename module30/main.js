d1c4f6f60d2e02fb2b307a11848e625bbe52d3b5
Dorant14 <dorant.hajdari12@gmail.com>
2024-10-24 16:18:46 +0200

    raaaaaaarararaar

---
+++ module30/main.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById("btn")
+var text = document.getElementById("text")
+var h1 = document.getElementsByClassName("USSR")
+var text2 = document.getElementById("IDK")
+var text2 = document.getElementById("IDk")
+var text2 = document.getElementById("IdK")
+
