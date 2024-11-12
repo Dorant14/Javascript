91edcade278459f8b5229657719d536d359a9364
Dorant14 <dorant.hajdari12@gmail.com>
2024-11-12 16:16:25 +0100

    jojo

--- module 34/module 34/main.js
+++ module 34/module 34/main.js
@@ -5,4 +5,17 @@ function printName(){
     console.log("Olsa");
 }
 
-printName();
+var colors = ["blue", "red", "green", "purple"];
+
+function changeBgColor(){
+    document.querySelector('body').style.background =  colot[Math.floor(Math.random()*coolors.length)];
+}
+
+var names = ["Dorant", "Olsa", "Daris", "Anik", "Eden", "Shpat", "Blin"]
+
+function changeName(){
+    document.querySelector('p').innerHTML = names[Math.round(Math.random() * (names.length-1))];
+}
+
+setIntervalt(changeBgColor, 500);
+setIntervalt(changeName, 500);
